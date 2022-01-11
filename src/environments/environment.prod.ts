6ff011b0ba1717aca1c6e388c50d665f5f7c8790
John Doe <sus_007@ymail.com>
2022-01-11 16:11:35 +0545

    Installs Firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'todo-firebase-45612',
+    appId: '1:1024878148400:web:7c93b9187e3358e2b1a6f5',
+    storageBucket: 'todo-firebase-45612.appspot.com',
+    locationId: 'asia-southeast2',
+    apiKey: 'AIzaSyBv-OW4fiO0RcJnlkM9Wy_Doeu3JLNeIyI',
+    authDomain: 'todo-firebase-45612.firebaseapp.com',
+    messagingSenderId: '1024878148400',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'todo-firebase-45612',
+    appId: '1:1024878148400:web:7c93b9187e3358e2b1a6f5',
+    storageBucket: 'todo-firebase-45612.appspot.com',
+    locationId: 'asia-southeast2',
+    apiKey: 'AIzaSyBv-OW4fiO0RcJnlkM9Wy_Doeu3JLNeIyI',
+    authDomain: 'todo-firebase-45612.firebaseapp.com',
+    messagingSenderId: '1024878148400',
+  },
   production: false
 };
 
@@ -14,3 +23,27 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+
+
+
+/*
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBv-OW4fiO0RcJnlkM9Wy_Doeu3JLNeIyI",
+  authDomain: "todo-firebase-45612.firebaseapp.com",
+  projectId: "todo-firebase-45612",
+  storageBucket: "todo-firebase-45612.appspot.com",
+  messagingSenderId: "1024878148400",
+  appId: "1:1024878148400:web:7c93b9187e3358e2b1a6f5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
